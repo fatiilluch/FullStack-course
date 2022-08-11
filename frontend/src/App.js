a0c624d76a9f494b2c3b3dc43132c30239eb89c0
Fatima Lluch <fatilluch@gmail.com>
2022-08-10 22:50:45 -0300

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,34 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     //console.log(e.target[0].value);
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((result) => result.json()) // esto devuelve si es success. Tambien devuelve una promesa
-      .then((data) => { console.log(data); }) // tenemos que resolver la promesa de arriba. data es info que manda el server
-      .catch((err) => { console.log(err);  }) // ocurre si la promesa fue rechazada
-    setWord("")    
-  }
+      .then((data) => {
+        console.log(data);
+      }) // tenemos que resolver la promesa de arriba. data es info que manda el server
+      .catch((err) => {
+        console.log(err);
+      }); // ocurre si la promesa fue rechazada
+    setWord("");
+  };
 
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit = {handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
