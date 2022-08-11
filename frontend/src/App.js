4af1e2fb9ff6dba3b05a700b79afc68d768aaeaa
Fatima Lluch <fatilluch@gmail.com>
2022-08-11 00:11:42 -0300

    show all images from the query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]); // cuando la aplicacion arranca no tenemos imagenes
-
-  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -20,7 +20,7 @@ function App() {
     )
       .then((result) => result.json()) // esto devuelve si es success. Tambien devuelve una promesa
       .then((data) => {
-        setImages([data, ...images]); // le pasamos un array de imagenes existente.
+        setImages([{ ...data, title: word }, ...images]); // le pasamos un array de imagenes existente.
       }) // tenemos que resolver la promesa de arriba. data es info que manda el server
       .catch((err) => {
         console.log(err);
@@ -32,6 +32,16 @@ function App() {
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      {/* Se itera por la lista de imagenes y se va mostrando cada imagen a medida que se busca en el input */}
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
