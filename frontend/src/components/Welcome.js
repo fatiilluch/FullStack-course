398fb987a3b960a1a2bec04ca19be41a6346f4b4
Fatima Lluch <fatilluch@gmail.com>
2022-08-12 19:39:52 -0300

    Add backend

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 const Welcome = () => (
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4">Fluid jumbotron</h1>
-      <p class="lead">
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Fluid jumbotron</h1>
+      <p className="lead">
         This is a simple application that retrieves photos using unsplash API.
         In order to start, enter any search term in the input field{" "}
       </p>
