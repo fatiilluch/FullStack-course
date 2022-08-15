22b8e594ecc7e8e0fa6f03d45430e225f4392c69
Fatima Lluch <fatilluch@gmail.com>
2022-08-15 16:33:56 -0300

    Enabled pylint and black vcode formatter in python

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
-from json import load
+""" Python """
+import os
 from flask import Flask, request
-import requests as r 
-import os 
+import requests as r
+
 from dotenv import load_dotenv
 from flask_cors import CORS
 
@@ -11,8 +12,9 @@ UNSPLASH_URL = "https://api.unsplash.com/photos/random/"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 
+
 if not UNSPLASH_KEY:
-  raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
+    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
@@ -20,21 +22,21 @@ CORS(app)
 app.config["DEBUG"] = DEBUG
 
 # retorna single image
-@app.route("/newImage") 
-def newImage():
-  word = request.args.get("query") # obtiene el parametro query
-  
-  payload = {
-    "query": word
-  }
-  headers = {
-    "Accept-Version": "v1",
-    "Authorization": "Client-ID {}".format(UNSPLASH_KEY)
-  }
-    
-  response = r.get(url=UNSPLASH_URL, headers=headers, params=payload)
-  data = response.json()
-  return data
+@app.route("/newImage")
+def new_image():
+    """ This funtion return a new image """
+    word = request.args.get("query") # obtiene el parametro query
+    payload = {
+      "query": word
+    }
+    headers = {
+      "Accept-Version": "v1",
+      "Authorization": f"Client-ID {UNSPLASH_KEY}"
+    }
+    response = r.get(url=UNSPLASH_URL, headers=headers, params=payload)
+    data = response.json()
+    return data
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050)
+    
